24692cc4b023e62842044e660b84302e71501c13
kolosovpetro <kolosovp94@gmail.com>
2024-11-23 23:29:14 +0100

    work in progress

---
+++ locals.tf
@@ -0,0 +1,43 @@
+locals {
+  app_services = {
+    dev = {
+      name     = "ase-dev-${var.prefix}"
+      location = "northeurope"
+    }
+    qa = {
+      name     = "ase-qa-${var.prefix}"
+      location = "northeurope"
+    }
+  }
+
+  frontend_port_name             = "front-port-443"
+  frontend_ip_configuration_name = "front-ip-config-${var.prefix}"
+  backend_pools = [
+    "backend-pool-dev",
+    "backend-pool-qa"
+  ]
+  backend_http_settings_name = "backend-http-settings-${var.prefix}"
+  http_listener_name         = "http-listener-${var.prefix}"
+  ssl_certificate_name       = "agwy.test.razumovsky.me.pfx"
+
+  routing_settings = [
+    {
+      request_routing_rule_name  = "rule-dev"
+      rule_type                  = "Basic"
+      http_listener_name         = "http-listener-dev"
+      backend_address_pool_name  = "backend-pool-dev"
+      backend_http_settings_name = "http-settings-dev"
+      priority                   = 1
+      host_name                  = "ase-dev-${var.prefix}.azurewebsites.net"
+    },
+    {
+      request_routing_rule_name  = "rule-qa"
+      rule_type                  = "Basic"
+      http_listener_name         = "http-listener-qa"
+      backend_address_pool_name  = "backend-pool-qa"
+      backend_http_settings_name = "http-settings-qa"
+      priority                   = 2
+      host_name                  = "ase-qa-${var.prefix}.azurewebsites.net"
+    }
+  ]
+}
--- main.tf
+++ main.tf
@@ -3,6 +3,13 @@ resource "azurerm_resource_group" "rg_app_gateway" {
   location = "northeurope"
 }
 
+resource "azurerm_virtual_network" "public" {
+  name                = "vnet-${var.prefix}"
+  address_space = ["10.0.0.0/24"]
+  location            = azurerm_resource_group.rg_app_gateway.location
+  resource_group_name = azurerm_resource_group.rg_app_gateway.name
+}
+
 resource "azurerm_service_plan" "service_plan" {
   name                = "asp-dev-qa-${var.prefix}"
   resource_group_name = azurerm_resource_group.rg_app_gateway.name
@@ -12,7 +19,8 @@ resource "azurerm_service_plan" "service_plan" {
 }
 
 resource "azurerm_windows_web_app" "app_service_dev" {
-  name                = "ase-dev-${var.prefix}"
+  for_each            = local.app_services
+  name                = "ase-${each.key}-${var.prefix}"
   resource_group_name = azurerm_resource_group.rg_app_gateway.name
   location            = azurerm_service_plan.service_plan.location
   service_plan_id     = azurerm_service_plan.service_plan.id
@@ -20,11 +28,97 @@ resource "azurerm_windows_web_app" "app_service_dev" {
   site_config {}
 }
 
-resource "azurerm_windows_web_app" "app_service_qa" {
-  name                = "ase-qa-${var.prefix}"
+resource "azurerm_public_ip" "app_gateway_front_ip" {
+  name                = "pip-agwy-front-ip-${var.prefix}"
+  location            = azurerm_resource_group.rg_app_gateway.location
   resource_group_name = azurerm_resource_group.rg_app_gateway.name
-  location            = azurerm_service_plan.service_plan.location
-  service_plan_id     = azurerm_service_plan.service_plan.id
-
-  site_config {}
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
+
+resource "azurerm_subnet" "app_gateway_subnet" {
+  name                 = "subnet-agwy-${var.prefix}"
+  resource_group_name  = azurerm_resource_group.rg_app_gateway.name
+  virtual_network_name = azurerm_virtual_network.public.name
+  address_prefixes = ["10.0.0.128/26"]
+}
+
+resource "azurerm_application_gateway" "main" {
+  name                = "agwy-${var.prefix}"
+  resource_group_name = azurerm_resource_group.rg_app_gateway.name
+  location            = azurerm_resource_group.rg_app_gateway.location
+
+  sku {
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "agwy-ipc-${var.prefix}"
+    subnet_id = azurerm_subnet.app_gateway_subnet.id
+  }
+
+  frontend_port {
+    name = local.frontend_port_name
+    port = 443
+  }
+
+  frontend_ip_configuration {
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.app_gateway_front_ip.id
+  }
+
+  dynamic "backend_address_pool" {
+    for_each = local.routing_settings
+    content {
+      name = backend_address_pool.value.backend_address_pool_name
+      fqdns = [backend_address_pool.value.host_name]
+    }
+  }
+
+  backend_http_settings {
+    name                  = local.backend_http_settings_name
+    cookie_based_affinity = "Disabled"
+    port                  = 443
+    protocol              = "Https"
+    request_timeout       = 60
+  }
+
+  dynamic "http_listener" {
+    for_each = local.routing_settings
+    content {
+      name                           = http_listener.value.http_listener_name
+      frontend_ip_configuration_name = local.frontend_ip_configuration_name
+      frontend_port_name             = local.frontend_port_name
+      protocol                       = "Https"
+      ssl_certificate_name           = local.ssl_certificate_name
+      host_name                      = http_listener.value.host_name
+    }
+  }
+
+  ssl_certificate {
+    name     = local.ssl_certificate_name
+    data = filebase64("${path.module}/${local.ssl_certificate_name}")
+    password = var.ssl_certificate_password
+  }
+
+  dynamic "request_routing_rule" {
+    for_each = local.routing_settings
+    content {
+      name                       = request_routing_rule.value.request_routing_rule_name
+      rule_type                  = request_routing_rule.value.rule_type
+      http_listener_name         = request_routing_rule.value.http_listener_name
+      backend_address_pool_name  = request_routing_rule.value.backend_address_pool_name
+      backend_http_settings_name = local.backend_http_settings_name
+      priority                   = request_routing_rule.value.priority
+    }
+  }
+}
+
+# resource "azurerm_network_interface_application_gateway_backend_address_pool_association" "nic-assoc" {
+#   for_each                = var.routing_settings
+#   network_interface_id    = each.value.network_interface_id
+#   ip_configuration_name   = each.value.ip_configuration_name
+#   backend_address_pool_id = one(azurerm_application_gateway.main.backend_address_pool).id
+# }
